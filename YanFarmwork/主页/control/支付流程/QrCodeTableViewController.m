08f4eba6c316615488c223ba5675bf246ccab5fd
yanhuaqiang <715685152@qq.com>
2019-09-12 17:48:51 +0800

    no message

--- YanFarmwork/主页/control/支付流程/QrCodeTableViewController.m
+++ YanFarmwork/主页/control/支付流程/QrCodeTableViewController.m
@@ -62,12 +62,12 @@
     self.tableView.tableHeaderView = [UIView new];
     self.tableView.bounces = NO;
     
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        
-//        PayResultViewController *resultVC = [[PayResultViewController alloc] initWithNibName:@"PayResultViewController" bundle:nil];
-//        resultVC.hidesBottomBarWhenPushed = YES;
-//        [self.navigationController pushViewController:resultVC animated:YES];
-//    });
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        PayResultViewController *resultVC = [[PayResultViewController alloc] initWithNibName:@"PayResultViewController" bundle:nil];
+        resultVC.hidesBottomBarWhenPushed = YES;
+        [self.navigationController pushViewController:resultVC animated:YES];
+    });
     
    
 }
